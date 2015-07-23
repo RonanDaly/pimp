d2678405f49d4fc5832f37e365a7fb7ddc934657
Simon Rogers <simon.rogers@glasgow.ac.uk>
2015-07-23 16:16:09 +0100

    sampler integration
    Former-commit-id: 58729272ef54738d859f2a450c3f9f914228eb6d
    Former-commit-id: cd7778d28978e47a3d109f509b8e44576e79dade

--- django_projects/pimp/test_sampler.py
+++ django_projects/pimp/test_sampler.py
@@ -21,7 +21,6 @@ if __name__ == '__main__':
 	peakset = frag_set.FragSet()
 	peakset.annotations = []
 
-	n = network_sampler.NetworkSampler(peakset)
 
 	#Following needs to be done for each peak
 	for p in list_of_peaks:
@@ -38,21 +37,25 @@ if __name__ == '__main__':
 			previous_pos = [i for i,n in enumerate(peakset.annotations) if n.name==simple_name]
 			if len(previous_pos) == 0:
 				peakset.annotations.append(peak_objects.Annotation(a.compound.formula,simple_name))
-				newmeasurement.annotations[peakset.annotations[-1]] = a.confidence
+				newmeasurement.annotations[peakset.annotations[-1]] = float(a.confidence)
 			else:
 				# check if this measurement has had this annotation before
 				this_annotation = peakset.annotations[previous_pos[0]]
 				if this_annotation in newmeasurement.annotations:
 					current_confidence = newmeasurement.annotations[this_annotation]
-					newmeasurement.annotations[this_annotation] = max(a.confidence,current_confidence)
+					newmeasurement.annotations[this_annotation] = max(float(a.confidence),current_confidence)
 				else:
-					newmeasurement.annotations[this_annotation] = a.confidence
+					newmeasurement.annotations[this_annotation] = float(a.confidence)
 				
 			# print '\t' + a.compound.name + '\t' + a.compound.formula + '\t' + str(a.confidence)
 			# Compress similar ones
 
 	print "Stored " + str(len(peakset.measurements)) + " peaks and " + str(len(peakset.annotations)) + " unique annotations"
 
-	n = network_sampler.NetworkSampler(peakset)
 
+
+	n = network_sampler.NetworkSampler(peakset)
+	n.initialise_sampler()
+	n.multiple_network_sample(1000)
+	n.dump_output(open('testoutput.txt','w'))
 
