6258d306baee4e1580bce8f66a96da585c5855b2
Rónán Daly <ronan.daly@glasgow.ac.uk>
2015-11-11 14:49:36 +0000

    Changed setupR.R to use Packrat
    Former-commit-id: 2ae0d760849eec6ee2a1ef2d0c7b2fbeca1281b4
    Former-commit-id: 32bec4cd226abbf52469bec75fbb9ae5d3d8772d

--- setupR.R
+++ setupR.R
@@ -1,12 +1,2 @@
-options('repos'=list(CRAN='http://cran.rstudio.com/', Bioconductor='http://www.bioconductor.org/packages/release/bioc', PuMP='http://puma.ibls.gla.ac.uk/R'))
-if (! 'devtools' %in% installed.packages()[,'Package']) {
-	install.packages('devtools')
-}
-library(devtools)
-install.packages('mzmatch.R', type="source")
-install(pkg='build/PiMP', dependencies=TRUE)
-install(pkg='build/PiMPDB', dependencies=TRUE)
-
-#source("http://bioconductor.org/biocLite.R")
-
-
+options('repos'=append(c(PuMP='http://puma.ibls.gla.ac.uk/R'), BiocInstaller::biocinstallRepos()))
+source('packrat/init.R')
