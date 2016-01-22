41ed6db6578809425d52092044b60a4bf31fb755
Rónán Daly <ronan.daly@glasgow.ac.uk>
2016-01-22 10:36:41 +0000

    Trying to work around logging package errors
    Former-commit-id: 6b9fc3c4d1e6904a824fa1bac2fe0f07a3ea208b
    Former-commit-id: bb4a51c18b65383bb3f15fe72356774d918aeb81

--- PiMP/R/zzz.R
+++ PiMP/R/zzz.R
@@ -1,5 +1,6 @@
 .onLoad <- function(libname = find.package("mzmatch.R"), pkgname = "mzmatch.R") {
 	heapsize <- getJavaHeapSize()
 	mzmatch.init(memorysize=heapsize, version.1=FALSE)
+	print('Turning on logging')
 	logging::basicConfig()
 }
