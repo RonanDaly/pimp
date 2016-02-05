01c42a80fe27993d3c23ae1ff779c7fe30917f38
Rónán Daly <ronan.daly@glasgow.ac.uk>
2016-02-05 13:27:06 +0000

    Changed setup code to easily create new database.
    Changed build.gradle to only depend on python dependences when doing database work
    and setupInitialUser.py to perform the user interaction to create the initial user.
    
    
    Former-commit-id: 2a76c83d440f4b6717a23e08a9d296c08cd25f10
    Former-commit-id: 6cd2c14d0929174a704be2f141111b31e6000848

--- django_projects/pimp/setupInitialUser.py
+++ django_projects/pimp/setupInitialUser.py
@@ -1,16 +1,34 @@
 import os
-import sys
 import django
+import getpass
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pimp.settings')
 django.setup()
 
-from django.contrib.auth.models import User;
+from django.contrib.auth.models import User
 
-User.objects.create_superuser(
-	sys.argv[1],
-	sys.argv[2],
-	sys.argv[3],
-	first_name=sys.argv[4],
-	last_name=sys.argv[5]
-)
+if __name__ == '__main__':
+    if 'PIMP_INITIAL_USERNAME' not in os.environ:
+        os.environ['PIMP_INITIAL_USERNAME'] = raw_input('Username: ')
+    if 'PIMP_INITIAL_EMAIL_ADDRESS' not in os.environ:
+        os.environ['PIMP_INITIAL_EMAIL_ADDRESS'] = raw_input('Email: ')
+    if 'PIMP_INITIAL_PASSWORD' not in os.environ:
+        password = getpass.getpass('Password: ')
+        passwordAgain = getpass.getpass('Password (again): ')
+        while password != passwordAgain:
+            print('Error: Your passwords did not match.')
+            password = raw_input('Password: ')
+            passwordAgain = raw_input('Password (again): ')
+        os.environ['PIMP_INITIAL_PASSWORD'] = password
+    if 'PIMP_INITIAL_FIRST_NAME' not in os.environ:
+        os.environ['PIMP_INITIAL_FIRST_NAME'] = raw_input('First name: ')
+    if 'PIMP_INITIAL_LAST_NAME' not in os.environ:
+        os.environ['PIMP_INITIAL_LAST_NAME'] = raw_input('Last name: ')
+
+    User.objects.create_superuser(
+        os.environ['PIMP_INITIAL_USERNAME'],
+        os.environ['PIMP_INITIAL_EMAIL_ADDRESS'],
+        os.environ['PIMP_INITIAL_PASSWORD'],
+        first_name=os.environ['PIMP_INITIAL_FIRST_NAME'],
+        last_name=os.environ['PIMP_INITIAL_LAST_NAME']
+    )
