c7aa1c385a8f45311c67684640ad646fe4a8992f
Joe Wandy <joe.wandy@glasgow.ac.uk>
2016-03-09 12:30:35 +0000

    wrong character encoding ...
    Former-commit-id: 7f1c94d6e1adfdf8100379e37b895e0b498cd812
    Former-commit-id: 6927577f30faf0dd68bee6d80901bfb6575ef6c7

--- django_projects/pimp/experiments/views.py
+++ django_projects/pimp/experiments/views.py
@@ -48,7 +48,7 @@ from experiments import tasks
 import timeit
 import pickle
 from django.views.decorators.cache import cache_page
-# from pimp.profiler import profile
+# from pimp.profiler import profile
 
 def experiment(request, project_id):
     class RequiredComparisonFormSet(BaseFormSet):
