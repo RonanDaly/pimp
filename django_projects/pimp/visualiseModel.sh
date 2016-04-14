56d88dcaa8e1e690454d8e06cedbc389cd244f75
Rónán Daly <ronan.daly@glasgow.ac.uk>
2016-04-14 14:14:12 +0100

    Updated model schema visualisation tools.
    Former-commit-id: f85f77a3a34b0e12403d8434da599d2c0f0775b3
    Former-commit-id: 9867a4d4264dbbbe7c12134fab8460c157a7078f

--- django_projects/pimp/visualiseModel.sh
+++ django_projects/pimp/visualiseModel.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# For this to work, GraphViz needs to be installed
+# Then the dependencies in django_projects/requirements_model_visualisation.txt need to be installed
+# pip install -r requirements_model_visualisation.txt
 # Mangle around the dot file to get the layout needed
 # This probably involves putting invisible edges (e.g. data_models_Dataset -> groups_models_SampleAttribute [style=invis])
 
@@ -8,4 +11,4 @@ honcho run python manage.py graph_models -g groups experiments data compound fra
 dot -Tpdf -opimpFrankVisualisation.pdf -Gsize=4.8,2.8\! -Gmargin=0 -Gratio=0.707 -Gdpi=300 pimpFrankVisualisation.dot
 
 honcho run python manage.py graph_models -g groups experiments data compound projects fileupload -o pimpVisualisation.dot
-dot -Tpdf -opimpVisualisation.pdf -Gsize=4.8,2.8\! -Gmargin=1 -Gratio=0.707 -Gdpi=300 pimpVisualisation.dot
+dot -Tpdf -opimpVisualisation.pdf -Gsize=4.8,2.8\! -Gmargin=0 -Gratio=0.707 -Gdpi=300 pimpVisualisation.dot
