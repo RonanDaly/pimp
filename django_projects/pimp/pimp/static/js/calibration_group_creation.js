e1bbd5845e51bff5c9281dc66ca15ac4b086dfa3
Joe Wandy <joe.wandy@glasgow.ac.uk>
2016-08-02 16:29:16 +0100

    Add validation when assigning samples to groups.
    Former-commit-id: 3fec455193a3d6d84559e54cc4967c443624a4b1
    Former-commit-id: 0594c15654d82dc209cf36112e233259a543d034

--- django_projects/pimp/pimp/static/js/calibration_group_creation.js
+++ django_projects/pimp/pimp/static/js/calibration_group_creation.js
@@ -1,4 +1,5 @@
 function main() {
+
     // Create the sampleList DataTable
     // same as group_creation
     var sampleList = $('#sample_list').DataTable({
@@ -17,6 +18,58 @@ function main() {
                 searchable: false
             }
         ]
+    });
+
+    // disable all Add buttons initially
+    $('#qc-add-btn').prop('disabled', true);
+    $('#blank-add-btn').prop('disabled', true);
+    $('#standard-add-btn').prop('disabled', true);
+
+    sampleList.on( 'select', function(e, dt, type, indexes) {
+
+        // get selected rows
+        var sampleData = sampleList.rows({
+            selected: true
+        }).data();
+
+        // check if selected rows contains mzxml or csv
+        var has_mzxml = false;
+        var has_csv = false;
+        $.each(sampleData, function (_, sample) {
+
+            var filename = sample[1];
+            var extension = filename.split('.').pop();
+            if (extension.toUpperCase() === 'MZXML') {
+                has_mzxml = true;
+            } else if (extension.toUpperCase() === 'CSV') {
+                has_csv = true;
+            }
+
+            // console.log("Selected " + filename + ', extension ' + extension +
+            //     ', has_mzxml ' + has_mzxml + ', has_csv ' + has_csv);
+
+        });
+
+        if (has_mzxml && has_csv) {
+            $('#qc-add-btn').prop('disabled', false);
+            $('#blank-add-btn').prop('disabled', false);
+            $('#standard-add-btn').prop('disabled', false);
+        } else if (has_mzxml) {
+            $('#qc-add-btn').prop('disabled', false);
+            $('#blank-add-btn').prop('disabled', false);
+            $('#standard-add-btn').prop('disabled', true);
+        } else if (has_csv) {
+            $('#qc-add-btn').prop('disabled', true);
+            $('#blank-add-btn').prop('disabled', true);
+            $('#standard-add-btn').prop('disabled', false);
+        }
+
+    }).on( 'deselect', function (e, dt, type, indexes) {
+
+        $('#qc-add-btn').prop('disabled', true);
+        $('#blank-add-btn').prop('disabled', true);
+        $('#standard-add-btn').prop('disabled', true);
+
     });
 
     $('[data-toggle=tooltip]').tooltip(); // Set the tooltips
@@ -41,9 +94,30 @@ function main() {
         $('[name=samplesattributes-TOTAL_FORMS]').val(numberSampleAttributes);
     }
 
+    function validateSample(button_id, sampleData) {
+
+        valid = true
+        $.each(sampleData, function (_, sample) {
+
+            var filename = sample[1];
+            var extension = filename.split('.').pop();
+            if (button_id === 'qc-add-btn' && extension.toUpperCase() === 'CSV') {
+                valid = false;
+            } else if (button_id == 'blank-add-btn' && extension.toUpperCase() === 'CSV') {
+                valid = false;
+            } else if (button_id == 'standard-add-btn' && extension.toUpperCase() === 'MZXML') {
+                valid = false;
+            }
+
+        });
+        return valid;
+
+    }
+
     // Buttons for attributes in the samples_attributes column
     $('#samples_attributes')
         .on('click', '.add_samples', function(e) { // Add the selected samples to the selected condition
+
             e.preventDefault();
 
             // Add the samples in the selected table rows
@@ -52,35 +126,43 @@ function main() {
                 selected: true
             });
 
-
             // Check if the user selected any samples - if not, don't try and add any to the attribute
             if (samplesToAssign[0].length > 0) {
 
                 var attributeDiv = $(this).parents('.panel');
                 var attributeName = $(attributeDiv).prop('id');
-
                 attributeDiv.find('.sample_placeholder').remove();
 
                 var sampleData = sampleList.rows({
                     selected: true
                 }).data();
 
-                $.each(sampleData, function (_, sample) {
-                    // Increment the TOTAL_FORMS variable for django's formset
-                    numberSampleAttributes++;
-                    $('[name=samplesattributes-TOTAL_FORMS]').val(numberSampleAttributes);
+                var button_id = e.currentTarget.id;
+                valid = validateSample(button_id, sampleData);
+                if (valid) {
 
-                    // Clone the template field
-                    var newSample = $($('#sampleattribute_form_template').clone().html().replace(/__prefix__/g, (numberSampleAttributes - 1)));
-                    newSample.css('display', 'none');
-                    newSample.appendTo(attributeDiv.children('.panel-body'));
-                    $('[name="samplesattributes-' + (numberSampleAttributes - 1) + '-projfile"]').val(sample[0]);
-                    $('[name="samplesattributes-' + (numberSampleAttributes - 1) + '-attribute"]').val(attributeName);
+                    $.each(sampleData, function (_, sample) {
+                        // Increment the TOTAL_FORMS variable for django's formset
+                        numberSampleAttributes++;
+                        $('[name=samplesattributes-TOTAL_FORMS]').val(numberSampleAttributes);
 
-                    attributeDiv.find('ol').append('<li data-form_id=' + (numberSampleAttributes - 1) + ' data-sample_id=' + sample[0] + ' data-polarity="' + sample[2] + '"><div class="sample_name" style="display:inline;">' + sample[1] + ' </div><a href="#" class="remove_sample">(Remove)</a></li>');
-                });
+                        // Clone the template field
+                        var newSample = $($('#sampleattribute_form_template').clone().html().replace(/__prefix__/g, (numberSampleAttributes - 1)));
+                        newSample.css('display', 'none');
+                        newSample.appendTo(attributeDiv.children('.panel-body'));
+                        $('[name="samplesattributes-' + (numberSampleAttributes - 1) + '-projfile"]').val(sample[0]);
+                        $('[name="samplesattributes-' + (numberSampleAttributes - 1) + '-attribute"]').val(attributeName);
 
-                samplesToAssign.remove().draw();
+                        attributeDiv.find('ol').append('<li data-form_id=' + (numberSampleAttributes - 1) + ' data-sample_id=' + sample[0] + ' data-polarity="' + sample[2] + '"><div class="sample_name" style="display:inline;">' + sample[1] + ' </div><a href="#" class="remove_sample">(Remove)</a></li>');
+                    });
+
+                    samplesToAssign.remove().draw();
+
+                } else {
+                    var msg = "Cannot insert these samples into the calibration group.";
+                    var msg_div = $('#error-msg').text(msg).show();
+                }
+
             } else { // no samples were selected when the add sample button was pushed
                 console.warn("The user tried to add samples to the attribute, but no samples were selected!");
             }
