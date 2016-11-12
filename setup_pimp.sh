ea2045493f8dd3fa71fd9a32ae433a827ac588e3
Rónán Daly <ronan.daly@glasgow.ac.uk>
2016-11-12 22:26:27 +0000

    Fixing problems with LFS
    Former-commit-id: f7afc545a8ea33331982268f1d06797d95c05cc0
    Former-commit-id: 899f7396147c95ea1bc40d3c0512dd8c38d157bf

--- setup_pimp.sh
+++ setup_pimp.sh
@@ -3,10 +3,10 @@
 set -a
 source django_projects/pimp/.env
 set +a
-python virtualenv/virtualenv.py --python=python2.7 venv
+python virtualenv/virtualenv.py --python=python2.7 venv 
 source venv/bin/activate
-pip install --no-cache-dir -r django_projects/requirements.txt
-pip install --no-cache-dir -r django_projects/requirements_frank.txt
-${PIMP_RSCRIPT_PATH} setupR.R --args --bootstrap-packrat
+pip install --no-cache-dir -r django_projects/requirements.txt || exit 1
+pip install --no-cache-dir -r django_projects/requirements_frank.txt || exit 1
+${PIMP_RSCRIPT_PATH} setupR.R --args --bootstrap-packrat || exit 1
 
 rm -r PiMP PiMPDB packrat/src/*
