dc7ad5bb158b0fafe31fad59155f9737744ee872
Rónán Daly <ronan.daly@glasgow.ac.uk>
2017-02-02 13:24:17 +0000

    Fixing bug in entrypoint
    Former-commit-id: c83df6553c8b716556c62b6705493b675f44c4a5
    Former-commit-id: f702f16ba0e749c1aed5b2a5b04b51383e71d81e

--- DockerBaseContext/docker-entrypoint.sh
+++ DockerBaseContext/docker-entrypoint.sh
@@ -5,7 +5,7 @@ if [ "$(id -u)" = '0' ]; then
 	DOCKER_SOCKET_GROUP_ID=$(gosu pimp stat -c %g /var/run/docker.sock)
     DOCKER_SOCKET_GROUP_NAME=$(getent group ${DOCKER_SOCKET_GROUP_ID} | cut -d ':' -f 1)
 
-    if [ -z ${DOCKER_SOCKET_GROUP_NAME+x} ]; then
+    if [ -z "${DOCKER_SOCKET_GROUP_NAME}" ]; then
         groupadd -g ${DOCKER_SOCKET_GROUP_ID} dockersocketgroup
         usermod -aG dockersocketgroup pimp
     else
@@ -15,7 +15,7 @@ if [ "$(id -u)" = '0' ]; then
     BIND_GROUP_ID=$(gosu pimp stat -c %g /home/pimp/media)
     BIND_GROUP_NAME=$(getent group ${BIND_GROUP_ID} | cut -d ':' -f 1)
 
-    if [ -z ${BIND_GROUP_NAME+x} ]; then
+    if [ -z "${BIND_GROUP_NAME}" ]; then
         groupadd -g ${BIND_GROUP_ID} bindgroup
         usermod -aG bindgroup pimp
     else
