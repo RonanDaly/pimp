f0ba164b6ed5edfcd3016b886318a345aa950bbf
Rónán Daly <ronan.daly@glasgow.ac.uk>
2017-02-15 17:13:11 +0000

    Fixing bug
    Former-commit-id: 4d23bf4f371d37cbbc86a59f245c7e16676436fc
    Former-commit-id: bbfc93a3ada254a8512fd4fdd414befac83ba74a

--- django_projects/pimp/frank/migrations/0010_auto_20170215_1630.py
+++ django_projects/pimp/frank/migrations/0010_auto_20170215_1630.py
@@ -5,7 +5,7 @@ from django.db import models, migrations
 from projects.views import create_frank_project_objects
 
 def create_missing_frank_objects(apps, schema_editor):
-    Project = apps.get_model('project', 'Project')
+    Project = apps.get_model('projects', 'Project')
     PimpProjectFrankExp = apps.get_model('frank', 'PimpProjectFrankExp')
     
     for project in Project.objects.all():
